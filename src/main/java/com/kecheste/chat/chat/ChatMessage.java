288b21ef9e013debc74eaf625adc30f4e0ca74a6
Abenezer <aben.ezer443@gmail.com>
2024-03-10 15:35:05 +0300

    Initial commit

---
+++ src/main/java/com/kecheste/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.kecheste.chat.chat;
+
+import lombok.*;
+
+import java.awt.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+}
